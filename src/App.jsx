6d162f29d6e6e8750273dee6d624372a1c9afbcd
Muhammad Ahsan <ahsan.nazim@netsoltech.com>
2023-08-10 15:50:43 +0500

    Animation & Comments Added In Navbar Comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
+import AboutMe from "./AboutMe/AboutMe";
 import Navbar from "./Navbar/Navbar";
 
 const App = () => {
   return (
     <>
-    <div className="w-full flex flex-col items-center justify-center h-full">
-      <div className="w-[80%] max-[1536px]:w-full">
+      <div className="container mx-auto">
         <Navbar />
       </div>
-      <div>
-       t magni incidunt itaque esse quasi culpa hic eos, ad consectetur qui alias voluptas accusamus? Esse reiciendis excepturi nisi consequuntur! Assumenda, expedita!
+      <div className="container mx-auto mt-[120px] px-[16px]">
+        <AboutMe />
       </div>
-    </div>
     </>
   );
 };
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BiSolidUser } from "react-icons/bi";
 import { BsTelephoneFill } from "react-icons/bs";
-import { FaBloggerB } from "react-icons/fa";
 import { GiCrossMark, GiSkills } from "react-icons/gi";
 import { HiPresentationChartLine } from "react-icons/hi";
 import hamburger from "../Assets/hamburger.svg";
@@ -39,57 +38,76 @@ const Navbar = () => {
   ];
   return (
     <>
-      {width > 660 ? (
-        <div className="w-full flex justify-start items-center px-8 h-[80px] bg-[#FFFAFA]">
-          <img
-            src={logo}
-            alt=""
-            className="w-[75px] h-[75px] object-contain mr-20"
-          />
-          <ul className="flex space-x-8 font-Poppins text-[#282828] text-[14px] font-normal">
-            {navLinks.map((item, index) => (
-              <button
-                key={index}
-                className="transition-all ease-in-out delay-0 flex items-center space-x-2 hover:text-[#FF4C60] cursor-pointer hover:-translate-y-1  hover:scale-110"
-              >
-                <span className="text-[14px]">{item.icon}</span>
-                <li>{item.text}</li>
-              </button>
-            ))}
-          </ul>
-        </div>
+      {width > 700 ? (
+        <>
+          {/* Desktop View Navbar--- */}
+          <nav className="w-full flex justify-start items-center px-8 h-[80px] bg-[#FFFAFA] fixed top-0">
+            <img
+              src={logo}
+              alt="programmingwithahsan_icon"
+              className="w-[140px] h-[140px] object-contain mr-10"
+            />
+            <ul className="flex space-x-8 font-Poppins text-[#282828] text-[14px] font-normal">
+              {navLinks.map((item, index) => (
+                <button
+                  key={index}
+                  className="transition-all ease-in-out delay-0 flex items-center space-x-2 hover:text-[#FF4C60] cursor-pointer hover:-translate-y-1  hover:scale-110"
+                >
+                  <span className="text-[14px]">{item.icon}</span>
+                  <li>{item.text}</li>
+                </button>
+              ))}
+            </ul>
+          </nav>
+        </>
       ) : (
         <>
-          {isOpen ? (
-            <div className="w-full bg-[#FFFAFA] fixed top-0 left-0 h-full">
-              <div
-                className="w-full flex justify-end p-6"
-                onClick={() => setIsOpen(false)}
-              >
-                <GiCrossMark className="w-[30px] h-[30px]" />
-              </div>
-              <div className="w-full h-80 flex justify-center items-center">
-                <ul className="flex flex-col items-start justify-center font-Poppins text-[#282828] text-[18px] font-normal space-y-4">
-                  {navLinks.map((item, index) => (
-                    <button
-                      key={index}
-                      className="transition-all ease-in-out delay-75 flex items-center space-x-2 hover:text-[#FF4C60] cursor-pointer hover:-translate-y-1 hover:scale-110"
-                    >
-                      <span className="text-[20px]">{item.icon}</span>
-                      <li>{item.text}</li>
-                    </button>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ) : (
+          {/* Mobile View Navbar Open--- */}
+          <nav
+            className={`transition-all ease-out duration-700 w-full bg-[#FFFAFA] fixed top-0 left-0 h-full ${
+              isOpen ? "" : "opacity-0 invisible"
+            }`}
+          >
             <div
-              className="w-full flex justify-end items-center h-[80px] px-8 fixed top-0 let-0 bg-[#FFFAFA]"
-              onClick={() => setIsOpen(true)}
+              className="w-full flex justify-between items-center px-2 pr-4 mt-[-20px]"
+              onClick={() => setIsOpen(false)}
             >
-              <img src={hamburger} alt="" />
+              <img
+                src={logo}
+                alt="programmingwithahsan_icon"
+                className="w-[140px] h-[140px] object-contain"
+              />
+              <GiCrossMark className="w-[30px] h-[30px]" />
             </div>
-          )}
+            <div className="w-full h-80 flex justify-center items-center">
+              <ul className="flex flex-col items-start justify-center font-Poppins text-[#282828] text-[32px] font-normal space-y-6">
+                {navLinks.map((item, index) => (
+                  <button
+                    key={index}
+                    className="transition-all ease-in-out delay-75 flex items-center space-x-2 hover:text-[#FF4C60] cursor-pointer hover:-translate-y-1 hover:scale-110"
+                  >
+                    <span className="text-[28px]">{item.icon}</span>
+                    <li>{item.text}</li>
+                  </button>
+                ))}
+              </ul>
+            </div>
+          </nav>
+
+          {/* Mobile View Navbar Close--- */}
+          <div
+            className={`transition-all ease-out duration-700 w-full flex justify-between items-center px-2 pr-4 mt-[-20px] fixed top-0 bg-[#FFFAFA] ${
+              isOpen ? "opacity-0 invisible" : ""
+            }`}
+            onClick={() => setIsOpen(true)}
+          >
+            <img
+              src={logo}
+              alt="programmingwithahsan_icon"
+              className="w-[140px] h-[140px] object-contain"
+            />
+            <img src={hamburger} alt="hamburger_icon" />
+          </div>
         </>
       )}
     </>
